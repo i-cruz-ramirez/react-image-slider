bedbf697731880d3f9d8f6d47bcf95f6fd814017
Isaac Cruz Ramirez <icruzr93@gmail.com>
2018-07-05 18:05:45 -0500

    Minimal Changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Slider from "./components/Slider.component";
 
-import "./styles.css";
-
-function App() {
-  return <Slider />;
-}
-
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Slider />, rootElement);
